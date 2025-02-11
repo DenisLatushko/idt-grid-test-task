aa09bd9f3509de8e2769aaa462a6f886cce9e3ee
Denis Latushko <denis.latushko92@gmail.com>
2025-02-11 11:47:07 +0300

    Attached data module to app to allow DI modules implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
 
 dependencies {
     implementation(projects.domain)
+    implementation(projects.data)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
