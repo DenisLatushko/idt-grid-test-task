2b8fc6157c08bca8f88d30a597eb2ced499963f5
Denis Latushko <denis.latushko92@gmail.com>
2025-02-11 14:21:35 +0300

    Added scope for grid builder contract classes

--- app/src/main/java/net/idt/testtask/grid/feature/gridbuilder/GridBuilderContract.kt
+++ app/src/main/java/net/idt/testtask/grid/feature/gridbuilder/GridBuilderContract.kt
@@ -1,11 +1,11 @@
 package net.idt.testtask.grid.feature.gridbuilder
 
-data class GridBuilderState(
+internal data class GridBuilderState(
     val hasColNumberError: Boolean = false,
     val hasRowNumberError: Boolean = false
 )
 
-sealed interface GridBuilderAction {
+internal sealed interface GridBuilderAction {
     class ApplySettings(
         val colNumberText: String,
         val rowNumberText: String
