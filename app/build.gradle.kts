0df4cae4caa38ab012f246923563b8ab51d0923b
Denis Latushko <denis.latushko92@gmail.com>
2025-02-11 11:39:07 +0300

    Attached domain module to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,8 @@ android {
 }
 
 dependencies {
+    implementation(projects.domain)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
