e7c045688aa5c1277bd998cc488a97b00c73541f
Denis Latushko <denis.latushko92@gmail.com>
2025-02-11 11:36:57 +0300

    Updated imports

--- app/src/test/java/net/idt/testtask/grid/ExampleUnitTest.kt
+++ app/src/test/java/net/idt/testtask/grid/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package net.idt.testtask.grid
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
