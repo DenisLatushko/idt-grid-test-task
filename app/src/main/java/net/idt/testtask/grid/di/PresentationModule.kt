0523756789f963f394851fe17bd73c93c24395d5
Denis Latushko <denis.latushko92@gmail.com>
2025-02-12 17:13:06 +0300

    Updated presentation DI module

--- app/src/main/java/net/idt/testtask/grid/di/PresentationModule.kt
+++ app/src/main/java/net/idt/testtask/grid/di/PresentationModule.kt
@@ -1,5 +1,9 @@
 package net.idt.testtask.grid.di
 
+import net.idt.testtask.domain.model.TextDomainModel
+import net.idt.testtask.grid.feature.grid.GridItemState
+import net.idt.testtask.grid.feature.grid.GridItemStateMapper
+import net.idt.testtask.grid.feature.grid.GridViewModel
 import net.idt.testtask.grid.feature.gridbuilder.GridBuilderViewModel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModel
@@ -7,4 +11,14 @@ import org.koin.dsl.module
 
 internal fun getPresentationModule(): Module = module {
     viewModel { GridBuilderViewModel(get()) }
+
+    viewModel { parameters ->
+        GridViewModel(
+            initParams = parameters.get(),
+            getGridTextUseCase = get(),
+            gridItemStateMapper = get()
+        )
+    }
+
+    factory<(TextDomainModel) -> GridItemState> { GridItemStateMapper() }
 }
