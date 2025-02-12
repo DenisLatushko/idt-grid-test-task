e01dd1b511deeb1044c58c94ebc453ecb414f8c8
Denis Latushko <denis.latushko@cogniteq.com>
2025-02-12 16:18:54 +0100

    Added navigation framework for compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.navigation)
 
     implementation(libs.bundles.koin)
 
