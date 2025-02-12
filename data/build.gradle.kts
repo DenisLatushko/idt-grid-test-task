c4574ed064f119cae4e99d62277e14d216ec50f3
Denis Latushko <denis.latushko92@gmail.com>
2025-02-12 11:58:42 +0300

    Updated build.gradle.kts for data module with necessary libs

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -4,4 +4,10 @@ plugins {
 
 dependencies {
     implementation(projects.domain)
+    implementation(libs.koin.core)
+    implementation(libs.kotlinx.coroutines.core)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
