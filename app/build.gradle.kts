143be6c8450d62666b2547ae3f5cba1f619a99ae
Denis Latushko <denis.latushko@cogniteq.com>
2025-02-12 20:29:56 +0100

    Updated frameworks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
 
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
