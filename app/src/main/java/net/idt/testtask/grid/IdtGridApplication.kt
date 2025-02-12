4395f0e8cbdcd109cef9c410744896be131eed87
Denis Latushko <denis.latushko92@gmail.com>
2025-02-12 14:25:21 +0300

    Updated koin initialization

--- app/src/main/java/net/idt/testtask/grid/IdtGridApplication.kt
+++ app/src/main/java/net/idt/testtask/grid/IdtGridApplication.kt
@@ -1,7 +1,8 @@
 package net.idt.testtask.grid
 
 import android.app.Application
-import net.idt.testtask.grid.di.getDomainModule
+import net.idt.testtask.di.getDataModule
+import net.idt.testtask.domain.di.getDomainModule
 import net.idt.testtask.grid.di.getPresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -19,6 +20,7 @@ class IdtGridApplication: Application() {
             if (BuildConfig.DEBUG) androidLogger()
             androidContext(this@IdtGridApplication)
             modules(
+                getDataModule(),
                 getDomainModule(),
                 getPresentationModule()
             )
